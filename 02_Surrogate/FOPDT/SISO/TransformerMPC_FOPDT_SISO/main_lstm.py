e2c1d2d73a51d332b5bbaf9c70f63d6d471930f2
Junho Park <jnho.park@gmail.com>
2022-03-08 12:00:27 -0700

    add full mpc_lstm

--- 02_Surrogate/FOPDT/SISO/TransformerMPC_FOPDT_SISO/main_lstm.py
+++ 02_Surrogate/FOPDT/SISO/TransformerMPC_FOPDT_SISO/main_lstm.py
@@ -1,25 +1,60 @@
 from siso_fopdt import *
-from mpc_fopdt import Mpc
+from mpc_lstm import Mpc
 
+
+import os
+
+path = os.path.dirname(os.path.realpath(__file__))
 
 import numpy as np
+import pandas as pd
+
 import matplotlib.pyplot as plt
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+
+from pickle import dump, load
+from sklearn.preprocessing import MinMaxScaler
 import time
-# from scipy.integrate import odeint
-import 
-from scipy.optimize import minimize
+
+# For LSTM model
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Dropout
+from keras.callbacks import EarlyStopping
+from keras.models import load_model
+
+
+# path = './'
+# Load NN model parameters and MinMaxScaler
+model_params = load(open('model_param.pkl', 'rb'))
+s1 = model_params['Xscale']
+s2 = model_params['yscale']
+window = model_params['window']
+
+# Load NN models (onestep prediction models)
+model_lstm = load_model('MPC_surrogate_SISO_FOPDT_LSTM.h5')
+# model_trans = load_model('MPC_surrogate_SISO_FOPDT_Transformer.h5')
+
+# Load NN models (multistep prediction models)
+model_lstm_multi = load_model('MPC_surrogate_SISO_FOPDT_multistep_LSTM.h5')
+# model_trans_multi = load_model('MPC_surrogate_SISO_FOPDT_multistep_Transformer.h5')
+
 
 # FOPDT Parameters
 K=3.0      # gain
 tau=5.0    # time constant
-ns = 100    # Simulation Length
+ns = 20    # Simulation Length
 t = np.linspace(0,ns,ns+1)
 delta_t = t[1]-t[0]
 
 
 # Define horizons
-P = 12 # Prediction Horizon
-M = 5  # Control Horizon
+P = 10 # Prediction Horizon
+M = 4  # Control Horizon
+
 
 # Input Sequence
 u = np.zeros(ns+1)
@@ -39,17 +74,26 @@ yp = np.zeros(ns+1)
 
 
 p = ProcessModel(K, tau, delta_t)
-m = Mpc(P, M, K, tau, delta_t)
+m = Mpc(window, P, M, s1, s2, multistep=1, model=model_lstm, model_multi=model_lstm_multi)
 
 uhat = np.zeros(M)
+for i in range(1, window):
+    yp[i] = p.run(u[i-1])
 
-for i in range(1,ns):
+
+
+
+u_window = u[0:window]
+y_window = yp[0:window]
+# SP_window = sp[0:window]
+
+for i in range(window,ns):
     print(i)
     # run process model
     yp[i+1] = p.run(u[i])
 
     # run MPC 
-    uhat = m.run(uhat, yp[i], sp[i])
+    uhat = m.run(uhat, u_window, y_window, sp[i])
     u[i+1] = uhat[0]
     delta = u[i+1] - u[i]
     
@@ -58,6 +102,11 @@ for i in range(1,ns):
             u[i+1] = u[i]+maxmove
         else:
             u[i+1] = u[i]-maxmove
+
+
+    u_window = u[i-window+1:i+1]
+    y_window = yp[i-window+1:i+1]
+    # SP_window = sp[i-window+1:i+1]    
 
     # else:
     #     u[i+1] = u[i]+delta[0]   
--- 02_Surrogate/FOPDT/SISO/TransformerMPC_FOPDT_SISO/mpc_lstm.py
+++ 02_Surrogate/FOPDT/SISO/TransformerMPC_FOPDT_SISO/mpc_lstm.py
@@ -1,31 +1,46 @@
-from hashlib import shake_128
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import time
 from scipy.integrate import odeint
 from scipy.optimize import minimize
 
+# For LSTM model
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Dropout
+from keras.callbacks import EarlyStopping
+from keras.models import load_model
+# from tqdm.keras import TqdmCallback
+
 
 class Mpc:
-    def __init__(self, window, P, M, dt, s1, s2,  model=None):
+    def __init__(self, window, P, M, s1, s2, multistep=False, model=None, model_multi=None):
         self.y_hat0 = 0
+        self.window = window
         self.P = P
         self.M = M
-        self.dt = dt
-        self.model = model
+        self.multistep = multistep
+        self.model_lstm = model
+        self.model_lstm_multi = model_multi
         self.s1 = s1 
         self.s2 = s2
-        self.window = window
+        
 
 
         # self.yp = yp
         # self.sp = sp
 
-    def MPCobj_lstm(u_hat, y_hat, SP_hat, u, y, SP, window, P, M, multistep):
+    def MPCobj_lstm(self, u_hat, u_window, y_window, sp):
         # future u values after the control horizon
-        u_hat_P = np.ones(P-M) * u_hat[-1]
-        u_all = np.concatenate((u, u_hat, u_hat_P),axis=None)
-        y_all = np.append(y, y_hat)
+        u_hat_P = np.ones(self.P-self.M) * u_hat[-1]
+        u_all = np.concatenate((u_window, u_hat, u_hat_P),axis=None)
+
+        y_hat = np.ones(self.P) * y_window[-1]
+        y_all = np.append(y_window, y_hat)
+
+        SP_hat = np.ones(self.P) * sp
 
         # X = pd.DataFrame({'u': u_all, 'y':y_all})
         # Y = pd.DataFrame({'y': y_all})
@@ -34,43 +49,45 @@ class Mpc:
         Y = np.transpose([y_all])
         SP_trans = np.transpose([SP_hat])
 
-        Xs = s1.transform(X)
-        Ys = s2.transform(Y)
-        SPs = s2.transform(SP_trans)
+        Xs = self.s1.transform(X)
+        Ys = self.s2.transform(Y)
+        SPs = self.s2.transform(SP_trans)
 
         # Appending the window (past) and Prediction (future) arrays
         Xsq = Xs.copy()
         Ysq = Ys.copy()
 
-        if multistep == 0:
+        if self.multistep == 0:
             # SPsq = np.reshape(SP_pred, (P,Ys.shape[1]))
-            for i in range(window,len(Xsq)):
-                Xin = Xsq[i-window:i].reshape((1, window, np.shape(Xsq)[1]))
+            for i in range(self.window,len(Xsq)):
+                Xin = Xsq[i-self.window:i].reshape((1, self.window, np.shape(Xsq)[1]))
                 # LSTM prediction
-                Xsq[i][(Xs.shape[1] - Ys.shape[1]):] = model_lstm(Xin) 
+                Xsq[i][(Xs.shape[1] - Ys.shape[1]):] = self.model_lstm(Xin) 
                 # (Xs.shape[1]-Ys.shape[1]) indicates the index of the 
                 # first 'system' output variable in the 'LSTM' input array
                 Ysq[i] = Xsq[i][(Xs.shape[1] - Ys.shape[1]):]
         
         else:
-            Xin = Xsq.reshape((1, window+P, np.shape(Xsq)[1]))
-            Ysq = model_lstm_multi(Xin)
+            Xin = Xsq.reshape((1, self.window+self.P, np.shape(Xsq)[1]))
+            Ysq = self.model_lstm_multi(Xin)
 
 
-        Ytu = s2.inverse_transform(Ysq)
-        Xtu = s1.inverse_transform(Xsq)
+        Ytu = self.s2.inverse_transform(Ysq)
+        Xtu = self.s1.inverse_transform(Xsq)
 
-        u_hat0 = np.append(u[-1], u_hat)
+        u_hat0 = np.append(u_window[-1], u_hat)
 
-        if multistep == 0:
-            pred_lstm["y_hat"] = np.reshape(Ytu[window:], (1,P))[0]
-            pred_lstm["u_hat"] = np.reshape(Xtu[window:,0], (1,P))[0]
+
+        pred_lstm = {}
+        if self.multistep == 0:
+            pred_lstm["y_hat"] = np.reshape(Ytu[self.window:], (1,self.P))[0]
+            pred_lstm["u_hat"] = np.reshape(Xtu[self.window:,0], (1,self.P))[0]
 
             Obj = 10*np.sum((pred_lstm["y_hat"] - SP_hat)**2) + np.sum(((u_hat0[1:] - u_hat0[0:-1])**2))
 
         else:
             pred_lstm["y_hat_multi"] = Ytu[0]
-            pred_lstm["u_hat_multi"] = Xtu[window:,0]
+            pred_lstm["u_hat_multi"] = Xtu[self.window:,0]
             
             Obj = 10*np.sum((pred_lstm["y_hat_multi"] - SP_hat)**2) + np.sum(((u_hat0[1:] - u_hat0[0:-1])**2))
 
@@ -79,17 +96,19 @@ class Mpc:
     
 
 
-    def run(self, ui, yp, sp):
+    def run(self,uhat, u_window, y_window, sp):
     # MPC calculation
 
         # u_hat0 = np.ones(self.M) * ui
         start = time.time()
 
-        solution = minimize(self.objective,ui,method='SLSQP', args=(yp,sp))
+        # solution = minimize(self.objective,ui,method='SLSQP', args=(yp,sp))
+        solution = minimize(self.MPCobj_lstm, uhat, method='SLSQP',args=(u_window, y_window, sp),options={'eps': 1e-06, 'ftol': 1e-01})
         u = solution.x  
 
         end = time.time()
         elapsed = end - start
+        print(elapsed)
 
         
 
