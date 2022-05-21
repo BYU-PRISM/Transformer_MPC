296ffe2d42a1658e819c40830f4b5cb433c4a48d
Junho Park <jnho.park@gmail.com>
2022-05-21 13:01:44 -0600

    update

--- 02_Surrogate/TCLab/03_TCLab_Controllers/main_nn.py
+++ 02_Surrogate/TCLab/03_TCLab_Controllers/main_nn.py
@@ -30,134 +30,136 @@ from keras.callbacks import EarlyStopping
 from keras.models import load_model
 
 import tclab
-TCLab = tclab.setup(connected=False, speedup=1)
+# TCLab = tclab.setup(connected=False, speedup=1)
 
-# lab = tclab.TCLab()
-with TCLab() as lab:
+lab = tclab.TCLab()
+# with TCLab() as lab:
 
-    # Load NN model parameters and MinMaxScaler
-    model_params = load(open('model_param_MIMO.pkl', 'rb'))
-    s1 = model_params['Xscale']
-    s2 = model_params['yscale']
-    window = model_params['window']
+# Load NN model parameters and MinMaxScaler
+model_params = load(open('model_param_MIMO.pkl', 'rb'))
+s1 = model_params['Xscale']
+s2 = model_params['yscale']
+window = model_params['window']
 
-    # Load NN models (onestep prediction models)
-    model_lstm_one = load_model('MPC_MIMO_TCLab_onestep_LSTM.h5')
-    model_trans_one = load_model('MPC_MIMO_TCLab_onestep_Trans.h5')
+# Load NN models (onestep prediction models)
+model_lstm_one = load_model('MPC_MIMO_TCLab_onestep_LSTM.h5')
+model_trans_one = load_model('MPC_MIMO_TCLab_onestep_Trans.h5')
 
-    # Load NN models (multistep prediction models)
-    model_lstm_multi = load_model('MPC_MIMO_TCLab_multistep_LSTM_110000.h5')
-    model_trans_multi = load_model('MPC_MIMO_TCLab_multistep_Trans_110000.h5')
+# Load NN models (multistep prediction models)
+model_lstm_multi = load_model('MPC_MIMO_TCLab_multistep_LSTM_110000.h5')
+model_trans_multi = load_model('MPC_MIMO_TCLab_multistep_Trans_110000.h5')
 
 
-    ns = window*30 + 10 * 60  # Simulation Length, min * 60
-    t = np.linspace(0, ns-1, ns)
+ns = 10 * 60  # Simulation Length, min * 60
+t = np.linspace(0, ns-1, ns)
 
-    nu = 2
-    ny = 2
+nu = 2
+ny = 2
 
-    # Define horizons
-    P = 10  # Prediction Horizon
-    M = 4  # Control Horizon
+# Define horizons
+P = 10  # Prediction Horizon
+M = 4  # Control Horizon
 
-    # Input Sequence
-    # u = np.zeros((ns, nu))
+# Input Sequence
+# u = np.zeros((ns, nu))
 
-    ## Set initial set_temp
-    sp1_init = lab.T1
-    sp2_init = lab.T2
+## Set initial set_temp
+sp1_init = lab.T1
+sp2_init = lab.T2
 
-    # Setpoint Sequence
-    sp1 = np.ones(ns) * sp1_init
-    sp1[window*30:] = 50
-    sp1[8*60:] = 40
+# Setpoint Sequence
+sp1 = np.ones(ns) * sp1_init
+sp1[window*30:] = 60
+sp1[6*60:] = 50
 
-    sp2 = np.ones(ns) * sp2_init
-    sp2[window*30:] = 30
+sp2 = np.ones(ns) * sp2_init
+sp2[window*30:] = 50
+sp2[7*60:] = 50
 
-    # Controller setting
-    maxmove = 1
 
-    # Process simulation
-    yp = np.zeros((ns, ny))
-    yp_nn = np.zeros((ns, ny, P))
+# Controller setting
+maxmove = 1
 
-    m = Mpc_nn(window, nu, ny, P, M, s1, s2, multistep=1, model_one=model_trans_one, model_multi=model_trans_multi)
+# Process simulation
+yp = np.zeros((ns, ny))
+yp_nn = np.zeros((ns, ny, P))
 
-    # Set initial fake data
-    TCL_data = pd.read_pickle('initial_fake_data.pkl')
-    TCL_data = TCL_data[0:window*30]
+m = Mpc_nn(window, nu, ny, P, M, s1, s2, multistep=1, model_one=model_trans_one, model_multi=model_trans_multi)
 
-    Q1_arr = TCL_data.iloc[:,0]
-    Q2_arr = TCL_data.iloc[:,1]
-    T1_arr = TCL_data.iloc[:,2]
-    T2_arr = TCL_data.iloc[:,3]
+# Set initial fake data
+TCL_data = pd.read_pickle('initial_fake_data.pkl')
+TCL_data = TCL_data[0:window*30]
 
-    T1_arr = T1_arr + (lab.T1 - np.average(T1_arr))
-    T2_arr = T2_arr + (lab.T2 - np.average(T2_arr))
+Q1_arr = TCL_data.iloc[:,0]
+Q2_arr = TCL_data.iloc[:,1]
+T1_arr = TCL_data.iloc[:,2]
+T2_arr = TCL_data.iloc[:,3]
 
-    uhat = np.zeros((M,nu))
+T1_arr = T1_arr + (lab.T1 - np.average(T1_arr))
+T2_arr = T2_arr + (lab.T2 - np.average(T2_arr))
 
-    lab.LED(70)
+uhat = np.zeros((M,nu))
 
-    for i in range(30*window, ns):
-        T1 = lab.T1
-        T2 = lab.T2
-        Q1 = lab.Q1()
-        Q2 = lab.Q2()
+lab.LED(70)
 
-        T1_arr = np.append(T1_arr,T1)
-        T2_arr = np.append(T2_arr,T2)
-        Q1_arr = np.append(Q1_arr,Q1)
-        Q2_arr = np.append(Q2_arr,Q2)
+for i in range(30*window, ns):
+    T1 = lab.T1
+    T2 = lab.T2
+    Q1 = lab.Q1()
+    Q2 = lab.Q2()
 
-        u = np.vstack((Q1_arr,Q2_arr)).T
-        y = np.vstack((T1_arr,T2_arr)).T
-        sp = np.array([sp1, sp2]).T
+    T1_arr = np.append(T1_arr,T1)
+    T2_arr = np.append(T2_arr,T2)
+    Q1_arr = np.append(Q1_arr,Q1)
+    Q2_arr = np.append(Q2_arr,Q2)
 
-        if i%30 == 0:
-            
-            u_window = u[-30*window::30]
-            y_window = y[-30*window::30]
-            
-            uhat = m.run(uhat, u_window, y_window, sp[i])
+    u = np.vstack((Q1_arr,Q2_arr)).T
+    y = np.vstack((T1_arr,T2_arr)).T
+    sp = np.array([sp1, sp2]).T
 
-            lab.Q1(uhat[0][0])
-            lab.Q2(uhat[0][1])
-            
-            print(uhat[0], "\n")
+    if i%5 == 0:
         
-        sec = i
-        Min = sec // 60
-        sec %= 60
-        print(Min, "min", sec, "sec   SP1:", sp1[i], " SP2:", sp2[i], 
-            "   T1:", T1_arr[i], "  T2:", T2_arr[i],
-            "    H1:", Q1_arr[i], "  H2:", Q2_arr[i])
+        u_window = u[-30*window::30]
+        y_window = y[-30*window::30]
         
-        time.sleep(1)
+        uhat = m.run(uhat, u_window, y_window, sp[i])
+
+        lab.Q1(uhat[0][0])
+        lab.Q2(uhat[0][1])
         
-    lab.LED(0)
-    lab.close()
+        print(uhat[0], "\n")
+    
+    sec = i
+    Min = sec // 60
+    sec %= 60
+    print(Min, "min", sec, "sec   SP1:", sp1[i], " SP2:", sp2[i], 
+        "   T1:", T1_arr[i], "  T2:", T2_arr[i],
+        "    H1:", Q1_arr[i], "  H2:", Q2_arr[i])
+    
+    time.sleep(1)
+    
+lab.LED(0)
+lab.close()
 
-    sec = ns
-    sec = str(sec)
+sec = ns
+sec = str(sec)
 
-    plt.figure()
-    plt.subplot(2, 1, 1)
-    plt.plot(t, y[:, 0], 'c-', label='T1')
-    plt.plot(t, y[:, 1], 'm-', label='T2')
-    plt.step(t, sp1, 'r--', label='SP1')
-    plt.step(t, sp2, 'g--', label='SP2')
-    plt.legend()
+plt.figure()
+plt.subplot(2, 1, 1)
+plt.plot(t, y[:, 0], 'c-', label='T1')
+plt.plot(t, y[:, 1], 'm-', label='T2')
+plt.step(t, sp1, 'r--', label='SP1')
+plt.step(t, sp2, 'g--', label='SP2')
+plt.legend()
 
-    plt.subplot(2, 1, 2)
-    plt.step(t, u[:, 0], 'r-', label='H1')
-    plt.step(t, u[:, 1], 'g-', label='H2')
-    plt.legend()
+plt.subplot(2, 1, 2)
+plt.step(t, u[:, 0], 'r-', label='H1')
+plt.step(t, u[:, 1], 'g-', label='H2')
+plt.legend()
 
-    plt.tight_layout()
+plt.tight_layout()
 
-    plt.show()
+plt.show()
 
 
-    print("break point")
+print("break point")
